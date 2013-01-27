c419b2a14966cc3b30db1c6cd990e1d50f56418b
julien <julien.genestoux@gmail.com>
2013-01-26 23:04:11 -0500

    removes all listed apps before adding them again

--- build/settings.subtome.js
+++ build/settings.subtome.js
@@ -1380,6 +1380,7 @@ $(document).ready(function() {
   });
 
   $('#suggestionsModal').on('hidden', function() {
+    $("#services tr").remove();
     services.forEach(addService);
   });
 
--- src/settings.js
+++ src/settings.js
@@ -38,6 +38,7 @@ $(document).ready(function() {
   });
 
   $('#suggestionsModal').on('hidden', function() {
+    $("#services tr").remove();
     services.forEach(addService);
   });
 
