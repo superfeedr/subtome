4871f7cc434a1bffd46fea1001a75777d66d27c2
julien <julien.genestoux@gmail.com>
2013-02-04 10:31:30 +0100

    loading the iframe from a non https resource as AWS S3 does not provide us any anyway

--- load.js
+++ load.js
@@ -1,8 +1,4 @@
 (function() {
-  var proto = 'https'
-  if (window.location.protocol != "https:")
-    proto = 'http'
-
   var feeds = [];
   var links = document.getElementsByTagName('link');
   for(var i = 0; i < links.length; i++) {
@@ -15,12 +11,11 @@
     }
   }
 
-
   var s = document.createElement('iframe');
   var url = window.location.toString();
   var resource = window.location.toString();
   s.setAttribute('style','position:absolute;top:0px; left:0px; width:100%; height:100%; border:0px; background: transparent; z-index: 2147483647');
-  s.setAttribute('src', proto + '://s3.amazonaws.com/www.subtome.com/subscribe.html?resource=' + encodeURIComponent(resource) + '&feeds=' + feeds.join(','));
+  s.setAttribute('src', 'http://www.subtome.com/subscribe.html?resource=' + encodeURIComponent(resource) + '&feeds=' + feeds.join(','));
   var loaded = false;
   s.onload = function() {
     if(loaded) {
