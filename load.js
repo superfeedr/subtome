544986375fe768e4389f67c6ea521740e5ed2279
Pelle Wessman <pelle@kodfabrik.se>
2013-03-17 19:42:13 +0100

    A single script can now serve many buttons
    All button, input and a tags with a class of `subtome` will be clickable if the script is loaded with a `standalone` parameter like:
    
    <script type="text/javascript">
      (function() {
        var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
        po.src = 'http://127.0.0.1:3000/load.js#standalone';
        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
      })();
    </script>
    
    Button as well as the script can have a `resource` and a `feed` parameter. The button defines them through data attributes like `data-resource` and the script using query parameters in the hash, like `http://example.com/load.js#resource=/about&feed=/feed.xml`. The parameters of the individual button overrides the global one of the script.
    
    The script is still backwards compatible with the old one - for those button it works like usual.

--- load.js
+++ load.js
@@ -1,16 +1,4 @@
-(function() {
-  var feeds = [];
-  var links = document.getElementsByTagName('link');
-  for(var i = 0; i < links.length; i++) {
-    if(links[i].rel) {
-      if(links[i].rel.split(' ').indexOf('alternate') >= 0) {
-        if(links[i].href && links[i].href.length > 0) {
-          feeds.push(encodeURIComponent(links[i].href));
-        }
-      }
-    }
-  }
-
+window.SubToMe = (function() {
   var getURLPart = function (url, part) {
     var e = document.createElement('a');
     getURLPart = function (url, part) {
@@ -20,38 +8,83 @@
     return getURLPart(url, part);
   };
 
+  var supportedHosts = ['www.subtome.com'];
+  var supportedParams = ['resource', 'feed'];
   var scriptParams = {};
   var scriptTag = document.querySelectorAll('script[src*="/load.js"]');
-  var scriptHash = scriptTag.length ? getURLPart(scriptTag[scriptTag.length - 1].src, 'hash') : '';
-  scriptHash = scriptHash === '' ? [] : scriptHash.substr(1).split('&');
+  var scriptHash;
+  var i = 0;
+  var length = scriptTag.length;
+  for (; i < length; i++) {
+    if (supportedHosts.indexOf(getURLPart(scriptTag[i].src, 'host')) !== -1) {
+      scriptHash = getURLPart(scriptTag[i].src, 'hash');
+      break;
+    }
+  }
+  scriptHash = scriptHash ? scriptHash.substr(1).split('&') : [];
 
   for (var j = 0, jLength = scriptHash.length; j < jLength; j++) {
     scriptHash[j] = scriptHash[j].split('=', 2);
     if (scriptHash[j][0] !== '') {
-      scriptParams[decodeURIComponent(scriptHash[j][0])] = scriptHash[j][1] ? encodeURIComponent(getURLPart(decodeURIComponent(scriptHash[j][1]), 'href')) : null;
+      scriptParams[decodeURIComponent(scriptHash[j][0])] = scriptHash[j][1] ? decodeURIComponent(scriptHash[j][1]) : null;
     }
   }
 
-  if (scriptParams.feed) {
-    var feedPosition = feeds.indexOf(scriptParams.feed);
-    if (feedPosition !== -1) {
-      feeds.splice(feedPosition, 1);
+  var showSubscribeFrame = function (params) {
+    var feeds = [];
+    var links = document.getElementsByTagName('link');
+    for(var i = 0; i < links.length; i++) {
+      if(links[i].rel) {
+        if(links[i].rel.split(' ').indexOf('alternate') >= 0) {
+          if(links[i].href && links[i].href.length > 0) {
+            feeds.push(encodeURIComponent(links[i].href));
+          }
+        }
+      }
     }
-    feeds.unshift(scriptParams.feed);
+
+    if (params.feed) {
+      params.feed = encodeURIComponent(getURLPart(params.feed, 'href'));
+      var feedPosition = feeds.indexOf(params.feed);
+      if (feedPosition !== -1) {
+        feeds.splice(feedPosition, 1);
+      }
+      feeds.unshift(params.feed);
+    }
+
+    var s = document.createElement('iframe');
+    var url = window.location.toString();
+    var resource = params.resource ? encodeURIComponent(getURLPart(params.resource, 'href')) : window.location.toString();
+    s.setAttribute('style','position:fixed;top:0px; left:0px; width:100%; height:100%; border:0px; background: transparent; z-index: 2147483647');
+    s.setAttribute('src', 'https://www.subtome.com/subscribe.html?resource=' + encodeURIComponent(resource) + '&feeds=' + feeds.join(','));
+    var loaded = false;
+    s.onload = function() {
+      if(loaded) {
+        document.getElementsByTagName('body')[0].removeChild(s);
+      }
+      loaded = true;
+    }
+    document.getElementsByTagName('body')[0].appendChild(s);
   }
 
-  var s = document.createElement('iframe');
-  var url = window.location.toString();
-  var resource = window.location.toString();
-  s.setAttribute('style','position:fixed;top:0px; left:0px; width:100%; height:100%; border:0px; background: transparent; z-index: 2147483647');
-  s.setAttribute('src', 'https://www.subtome.com/subscribe.html?resource=' + encodeURIComponent(resource) + '&feeds=' + feeds.join(','));
-  var loaded = false;
-  s.onload = function() {
-    if(loaded) {
-      document.getElementsByTagName('body')[0].removeChild(s);
+  if (scriptParams.standalone === undefined) {
+    showSubscribeFrame(scriptParams);
+  } else {
+    var buttons = document.querySelectorAll('button.subtome,input.subtome,a.subtome');
+    for (i = 0, length = buttons.length; i < length; i++) {
+      buttons[i].onclick = function () {
+        params = {};
+        for (i = 0, length = supportedParams.length; i < length; i++) {
+          params[supportedParams[i]] = this.getAttribute('data-' + supportedParams[i]) || scriptParams[supportedParams[i]] || undefined;
+        }
+        showSubscribeFrame(params);
+        return false;
+      };
     }
-    loaded = true;
   }
-  document.getElementsByTagName('body')[0].appendChild(s);
+
+  return {
+    showSubscribeFrame : showSubscribeFrame
+  };
 })();
 
