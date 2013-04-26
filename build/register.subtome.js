ce5170d9c3d1850427124dd80f72ebb4c777563c
julien <julien.genestoux@gmail.com>
2013-04-26 22:51:39 +0200

    adding a register-no-ui

--- build/register.subtome.js
+++ build/register.subtome.js
@@ -1376,12 +1376,6 @@ var url = urlParser.parse(window.location.href);
 var qs = qsParser.parse(url.query);
 
 services.register(qs.name, qs.url);
-$(document).ready(function() {
-  $('.serviceName').text(qs.name);
-  if(typeof(_gaq) != 'undefined') {
-    _gaq.push(['_trackEvent', 'Services', 'Register', qs.name, qs.name]);
-  }
-});
 
 });
 require("/register.js");
--- src/register.js
+++ src/register.js
@@ -7,9 +7,3 @@ var url = urlParser.parse(window.location.href);
 var qs = qsParser.parse(url.query);
 
 services.register(qs.name, qs.url);
-$(document).ready(function() {
-  $('.serviceName').text(qs.name);
-  if(typeof(_gaq) != 'undefined') {
-    _gaq.push(['_trackEvent', 'Services', 'Register', qs.name, qs.name]);
-  }
-});
