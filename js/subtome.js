18fdf1c3bba5e3c7a669c9b100532c3a6b1a06b2
julien <julien.genestoux@gmail.com>
2013-06-08 00:28:54 +0200

    loading the gists

--- js/subtome.js
+++ js/subtome.js
@@ -1,14 +1,28 @@
 angular.module('subtome', []).
-  config(['$routeProvider', function($routeProvider) {
+config(['$routeProvider', function($routeProvider) {
   $routeProvider.
-      when('/', {templateUrl: 'partials/index.html',   controller: IndexCtrl}).
-      when('/settings', {templateUrl: 'partials/settings.html', controller: SettingsCtrl}).
-      when('/publishers', {templateUrl: 'partials/publishers.html', controller: PublishersCtrl}).
-      when('/developers', {templateUrl: 'partials/developers.html', controller: DevelopersCtrl}).
-      when('/store', {templateUrl: 'partials/store.html', controller: StoreCtrl}).
-      otherwise({redirectTo: '/'});
-}]);
+    when('/', {templateUrl: 'partials/index.html',   controller: IndexCtrl}).
+    when('/settings', {templateUrl: 'partials/settings.html', controller: SettingsCtrl}).
+    when('/publishers', {templateUrl: 'partials/publishers.html', controller: PublishersCtrl}).
+    when('/developers', {templateUrl: 'partials/developers.html', controller: DevelopersCtrl}).
+    when('/store', {templateUrl: 'partials/store.html', controller: StoreCtrl}).
+    otherwise({redirectTo: '/'});
+  }
+]);
 
+
+function loadGists() {
+  $('.script').each(function(i,div) {
+    $.ajax({
+      url: $(div).data('src') + 'on',
+      dataType: 'jsonp',
+      success: function(gist) {
+        $('head').append('<link rel="stylesheet" href="' + gist.stylesheet + '" type="text/css" />');
+        $(div).append($(gist.div))
+      }
+    });
+  });
+}
 
 function IndexCtrl($scope, $routeParams) {
 // Don't load anything :)
@@ -19,11 +33,11 @@ function SettingsCtrl($scope, $routeParams) {
 }
 
 function PublishersCtrl($scope, $routeParams) {
-// Load the gists!
+  loadGists();
 }
 
 function DevelopersCtrl($scope, $routeParams) {
-  // Load the gists!
+  loadGists();
 }
 
 function StoreCtrl($scope, $routeParams) {
