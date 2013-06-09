d3169fc31aa6451fe9a22be7542da5aadace6acd
julien <julien.genestoux@gmail.com>
2013-06-09 21:36:41 +0200

    cleaned up some files

--- src/register.js
+++
@@ -1,9 +0,0 @@
-require('./compatibility.js');
-var urlParser = require('url');
-var qsParser = require('querystring');
-var services = require('./services');
-
-var url = urlParser.parse(window.location.href);
-var qs = qsParser.parse(url.query);
-
-services.register(qs.name, qs.url);
