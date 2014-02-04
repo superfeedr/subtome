c13c19f4331295d9573580e578020b043fdc3d37
julien <julien.genestoux@gmail.com>
2014-02-04 18:23:43 +0100

    merging with yeoman, grunt and bower based arch

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('subtomecomApp', [
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
